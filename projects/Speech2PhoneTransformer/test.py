6c79a557506eb2f273667de31537c5832986397a
ElCresp0 <jacky8203@protonmail.com>
2024-12-02 23:34:45 +0100

    add testing

--- projects/Speech2PhoneTransformer/test.py
+++ projects/Speech2PhoneTransformer/test.py
@@ -2,11 +2,16 @@ import pathlib
 from argparse import ArgumentParser
 
 import torch
+import torchaudio
 
-from data import DataHandler
+from data import PADDING, DataHandler
+from model import PinyinTransformer
+from torch import nn
+from training import N_TOKENS
+
 
 class Main:
-    def __init__(self, batch_size, num_workers):
+    def __init__(self, model_path: pathlib.Path, n_inputs: int, n_hidden: int, n_layers: int, batch_size: int):
         print("generating index")
         data_handler = DataHandler(
             phone_set_path=pathlib.Path("AISHELL-3/phone-set.txt"),
@@ -22,6 +27,28 @@ class Main:
             num_workers = 0
             pin_memory = False
 
+        # create model object
+        n_head = 1 # nb of heads for multiheaded attention
+        self.model = PinyinTransformer(N_TOKENS, n_inputs, n_head, n_hidden, n_layers).double().to(self.device)
+
+        # load the model
+        self.load_model(model_path)
+
+        test_set = data_handler.get_set("test")
+        _, sample_rate, _ = test_set[0]
+        print("done generating sets")
+
+        self.loss_criterion = nn.CrossEntropyLoss(ignore_index=data_handler.index.phon2idx[PADDING])
+
+        # loading waveform from .wav:
+        #   waveform, sample_rate = torchaudio.load("audio_file.wav", normalize=True)
+        new_sample_rate = 256 # 8000 # 16000 # original: 44100
+        self.transform_resample = torchaudio.transforms.Resample(orig_freq=sample_rate, new_freq=new_sample_rate)
+        self.transform_mfcc = torchaudio.transforms.MFCC(sample_rate=new_sample_rate, n_mfcc=n_inputs, melkwargs={"n_mels": n_inputs}) # (sample_rate=16000, n_mfcc=13, melkwargs={"n_fft": 400, "hop_length": 80, "n_mels": 23, "center": False}) # n_mfcc=13 (default:40), , log_mels=True
+
+        self.transform_resample = self.transform_resample.double().to(self.device)
+        self.transform_mfcc = self.transform_mfcc.double().to(self.device)
+
         self.test_loader = torch.utils.data.DataLoader(
             test_set,
             batch_size=batch_size,
@@ -31,6 +58,18 @@ class Main:
             num_workers=num_workers,
             pin_memory=pin_memory,
         )
+
+        self.run()
+
+
+    def load_model(self, model_path: pathlib.Path):
+        checkpoint = torch.load(model_path) # str(model_path)
+        self.model.load_state_dict(checkpoint['model_state_dict'])
+        self.optimizer.load_state_dict(checkpoint['optimizer_state_dict'])
+        self.scheduler.load_state_dict(checkpoint['scheduler_state_dict'])
+        self.epoch = checkpoint['epoch']
+        self.loss = checkpoint['loss']
+        print("loaded the model successfully")
 
     def run(self):
         with torch.no_grad():
